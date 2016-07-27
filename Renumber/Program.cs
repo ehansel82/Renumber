88b5be7d48e27380d28b6bbf1d82b0b2d7d1fb3f
Eric Hansel <e.hansel.01@gmail.com>
2016-07-26 20:25:58 -0400

    First proof of concept.

---
+++ Renumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Speech.Synthesis;
+
+namespace Renumber
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+
+            var random = new Random();
+            var numbers = new List<int>();
+
+            synth.Speak("Hi. How many numbers do you want to remember?");
+            var numbersToRemember = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numbersToRemember; i++)
+            {
+                numbers.Add(random.Next(99));
+            }
+
+            synth.Speak("Remember these numbers.");
+            foreach (var number in numbers)
+            {
+                synth.Speak(number.ToString());
+            }
+            for (int i = 0; i < numbersToRemember; i++)
+            {
+                var entry = Console.ReadLine();
+                if (entry != numbers[i].ToString())
+                {
+                    synth.Speak("WRONG! Please play again.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+            synth.Speak("YOU WIN! YAY!");
+            Console.ReadLine();
+        }
+    }
+}
